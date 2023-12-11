5da8b24cada9c07056c92dba38a450df7b426dab
Felix <sillsphelyx@gmail.com>
2023-12-11 05:57:32 -0800

    added new files to start the launch

---
+++ kachi.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+#from flask_sqlalchemy import SQLAlchemy
+#from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+#    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+#    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('about.html')
+
+@app.route('/numbers')
+def numbers():
+    return render_template('numbers.html')
+
+@app.route('/poetry') #/<int:post_id>')
+def poetry():
+
+    return render_template('poetry.html')
+@app.route('/sport')
+def sport():
+
+    return render_template('sport.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
